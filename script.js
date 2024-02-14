4d4e9b100897ab4767cf4602d74d2474fceec1ff
Raul Vinicius <raullwx@gmail.com>
2024-02-14 15:39:15 -0400

    new feature: adding a reset button

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
-document.querySelector('.calculate').addEventListener("click", function() {
+const calculateButton = document.querySelector('.calculate');
+const resetButton = document.querySelector('.reset');
+
+const resultX = document.querySelector('.result');
+
+let result = 0;
+
+calculateButton.addEventListener("click", function() {
 
     const num1 = document.querySelector('.num1').value;
     const num2 = document.querySelector('.num3').value;
     const num3 = document.querySelector('.num3').value;
 
     if (num1 === '' || num2 === '' || num3 === '' || num1 === 0) {
-        document.querySelector('.result').textContent = 'Inválido';
+        resultX.textContent = 'Inválido';
+
+        calculateButton.classList.remove("block");
+        calculateButton.classList.add("none");
+    
+        resetButton.classList.remove("none");
+        resetButton.classList.add("block");
         return;
     }
 
-    let result = (num3 * num2)/ num1;
+    result = (num3 * num2)/ num1;
 
     if (Number.isInteger(result)) {
-        document.querySelector('.result').textContent = result;
+        resultX.textContent = result;
     } else {
-        document.querySelector('.result').textContent = result.toFixed(2).replace('.', ',');
+        resultX.textContent = result.toFixed(2).replace('.', ',');
     }
+
+    calculateButton.classList.remove("block");
+    calculateButton.classList.add("none");
+    
+    resetButton.classList.remove("none");
+    resetButton.classList.add("block");
 });
+
+resetButton.addEventListener("click", function() {
+
+    result = 0;
+
+    clearInputUrlNumberText("entrada");
+
+    const itemX = document.createElement("i");
+    itemX.classList.add("fa-solid");
+    itemX.classList.add("fa-xmark");
+
+    resultX.textContent = '';
+    resultX.appendChild(itemX);
+
+    calculateButton.classList.remove("none");
+    calculateButton.classList.add("block");
+    
+    resetButton.classList.remove("block");
+    resetButton.classList.add("none");
+});
+
+function clearInputUrlNumberText(name) {
+    const entradas = document.querySelectorAll("input[name='"+name+"']");
+    [].map.call(entradas, entrada => entrada.value = '');
+};
